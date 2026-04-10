2d06476cf89dca7a4e3bfc3d36eb5af9886f71b0
Derek-chs <derekchs02@gmail.com>
2026-04-10 00:28:03 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,6 @@
-function showSection(sectionId) {
-  document.querySelectorAll('.section').forEach(sec => {
-    sec.style.display = 'none';
-  });
-
-  document.getElementById(sectionId).style.display = 'block';
-}
-
-function toggleMenu() {
-  showSection('menu');
-
-  let menu = document.getElementById('menuOptions');
-  menu.style.display = menu.style.display === 'block' ? 'none' : 'block';
-}
-
-function reservar() {
-  window.open('https://wa.me/50688888888', '_blank');
-}
-
 function abrirModal(nombre, descripcion, imagen) {
   let modal = document.getElementById("modal");
-  modal.style.display = "flex";
+  modal.classList.add("active");
 
   document.getElementById("titulo").innerText = nombre;
   document.getElementById("descripcion").innerText = descripcion;
@@ -27,5 +8,5 @@ function abrirModal(nombre, descripcion, imagen) {
 }
 
 function cerrarModal() {
-  document.getElementById("modal").style.display = "none";
+  document.getElementById("modal").classList.remove("active");
 }
